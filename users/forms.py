a1601964e901dc2d1508c70da384f13d08e8c6f5
Angelina Donchak <geliadonchak@mail.ru>
2021-01-05 14:24:43 +1000

    russification of register form

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import password_validation
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile, Grant
@@ -6,13 +7,28 @@ from dal import autocomplete
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
-
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        labels = {
+            'username': 'Имя пользователя',
+        }
+
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=100, label='Имя, Очество')
+    last_name = forms.CharField(max_length=100, label='Фамилия')
+    password1 = forms.CharField(
+        label='Пароль',
+        strip=False,
+        widget=forms.PasswordInput,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label='Подтверждение пароля',
+        widget=forms.PasswordInput,
+        strip=False,
+        help_text='Повторите пароль для подверждения',
+    )
 
 
 class UserUpdateForm(forms.ModelForm):
@@ -21,13 +37,6 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
-
-    # def save(self, username, commit=True):
-    #     instance = super(UserUpdateForm, self).save(commit=False)
-    #     instance.username = username
-    #     if commit:
-    #         instance.save()
-    #     return instance
 
     def save(self, username, commit=True):
         instance = super(UserUpdateForm, self).save(commit=False)
@@ -38,10 +47,18 @@ class UserUpdateForm(forms.ModelForm):
 
 
 class ProfileUpdateForm(forms.ModelForm):
-
     class Meta:
         model = Profile
         fields = ['image', 'organization', 'position', 'degree', 'biography', 'link', 'ORCID']
+        labels = {
+            'image': 'Изображение',
+            'organization': 'Организация',
+            'position': 'Должность',
+            'degree': 'Ученая степень',
+            'biography': 'Биография',
+            'link': 'Ссылка',
+            'ORCID': 'ORCID',
+        }
         widgets = {
             'organization': autocomplete.ModelSelect2(url='organization-autocomplete')
         }
@@ -55,7 +72,6 @@ class ProfileUpdateForm(forms.ModelForm):
 
 
 class GrantForm(forms.ModelForm):
-
     class Meta:
         model = Grant
         fields = ['name', 'year_from', 'year_to', 'fund', 'amount', 'link']
